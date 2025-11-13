d1a5502a63feb75a759ff38b95c0363ee492c406
Anjali <anjali.sankar@gojek.com>
2025-11-14 00:30:58 +0530

    fix

---
+++ src/main/java/com/gopay/app/models/GitlabEnvironmentResponse.java
@@ -0,0 +1,22 @@
+package com.gopay.app.models;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
+public class GitlabEnvironmentResponse {
+    @SerializedName("last_deployment")
+    private LastDeployment lastDeployment;
+
+    @Data
+    public static class LastDeployment {
+        private String sha;
+        private Pipeline pipeline;
+
+        @Data
+        public static class Pipeline {
+            @SerializedName("web_url")
+            private String webUrl;
+        }
+    }
+}
