a89330052cccbceaa5bb6b9d806e83651c1ec552
Anjali Sankar <anjali.sankar@gojek.com>
2025-11-13 12:54:20 +0000

    [Anjali]Gitlab second API changes

--- src/main/java/com/gopay/app/clients/httpclients/GitlabServiceInterface.java
+++ src/main/java/com/gopay/app/clients/httpclients/GitlabServiceInterface.java
@@ -1,11 +1,13 @@
 package com.gopay.app.clients.httpclients;
 
 
+import com.gopay.app.models.GitlabCompareResponse;
 import com.gopay.app.models.GitlabPipelineResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface GitlabServiceInterface {
     @GET("/api/v4/projects/{project}/pipelines/{pipelineId}")
@@ -13,5 +15,13 @@ public interface GitlabServiceInterface {
             @Header("PRIVATE-TOKEN") String privateToken,
             @Path("project") String project,
             @Path("pipelineId") long pipelineId
+    );
+
+    @GET("/api/v4/projects/{project}/repository/compare")
+    Call<GitlabCompareResponse> compareCommits(
+            @Header("PRIVATE-TOKEN") String privateToken,
+            @Path("project") String project,
+            @Query("from") String fromCommit,
+            @Query("to") String toCommit
     );
 }
---
+++ src/main/java/com/gopay/app/models/GitlabCompareResponse.java
@@ -0,0 +1,14 @@
+package com.gopay.app.models;
+
+
+public class GitlabCompareResponse {
+    private String web_url;
+
+    public String getWebUrl() {
+        return web_url;
+    }
+
+    public void setWebUrl(String web_url) {
+        this.web_url = web_url;
+    }
+}
