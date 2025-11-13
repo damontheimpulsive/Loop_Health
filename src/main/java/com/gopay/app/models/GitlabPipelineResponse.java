4f2cf628b9cdb8b333d0176912a58f6d974839ba
Anjali <anjali.sankar@gojek.com>
2025-11-13 20:24:27 +0530

    Fix

--- src/main/java/com/gopay/app/models/GitlabPipelineResponse.java
+++ src/main/java/com/gopay/app/models/GitlabPipelineResponse.java
@@ -1,9 +1,11 @@
 package com.gopay.app.models;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
 public class GitlabPipelineResponse {
     private String sha;
+    @SerializedName("web_url")
     private String webUrl;
 }
